ca3457b28b1ab6cc4c848753957f0124fbd7033d
Hong Phuc Le <72754942+lhplhp10102004@users.noreply.github.com>
2024-11-16 00:48:35 +0700

    Enhance Announcer class with threading and configurable IP; update configs for new tracker URL

--- announcer.py
+++ announcer.py
@@ -1,7 +1,42 @@
+import threading
+import time
+import schedule
+
+
 class Announcer:
-    def __init__(self, tracker_url, peer_id):
+    def __init__(self, tracker_url, peer_id, ip, refresh_interval=300):
         self.tracker_url = tracker_url
         self.peer_id = peer_id
+        self.refresh_interval = refresh_interval
+        self.ip = ip
+        self.thread = None
+        self.stop_triggered = False
 
     def announce(self):
-        print("Announcing to tracker...")
+        print("Announcing to tracker with ip: ", self.ip)
+
+    def announce_every_interval(self):
+        while not self.stop_triggered:
+            schedule.run_pending()
+            time.sleep(1)
+
+    def start(self):
+        self.announce()
+        schedule.every(self.refresh_interval).seconds.do(self.announce)
+        if self.thread is None:
+            self.thread = threading.Thread(target=self.announce_every_interval)
+            self.thread.start()
+
+    def stop(self):
+        if self.thread is not None:
+            self.stop_triggered = True
+            self.thread.join()
+            self.thread = None
+            self.stop_triggered = False
+
+
+if __name__ == "__main__":
+    announcer = Announcer("http://localhost:3000", "peer_id", "localhost", 3)
+    announcer.start()
+    time.sleep(10)
+    announcer.stop()
--- program.py
+++ program.py
@@ -3,6 +3,7 @@ from configuration import Configuration
 from announcer import Announcer
 from Modules.PeerConnection.seeding_pieces_manager import SeedingPiecesManager
 from Modules.PeerConnection.download_torrent_manager import DownloadTorrentManager
+import threading
 
 
 class Program:
@@ -10,31 +11,43 @@ class Program:
         self.ip = socket.gethostbyname(socket.gethostname())
         self.configs = Configuration()
         self.port = self.configs.port
-        self.announcer = Announcer(self.configs.tracker_url, self.configs.peer_id)
+        self.announcer = Announcer(
+            self.configs.tracker_url,
+            self.configs.peer_id,
+            self.ip,
+            5,
+        )
         self.seeding_pieces_manager = SeedingPiecesManager()
         self.download_torrent_manager = DownloadTorrentManager(
             self.configs.download_dir, self
         )
+        self.announcer.start()
 
     def start(self):
-        option = -1
-        while option != 0:
-            print("1. Create Torrent")
-            print("2. Download Torrent")
-            print("0. Exit")
-            option = int(input("Select an option: "))
-            if option == 1:
-                print("Creating torrent...")
-            elif option == 2:
-                print("Downloading torrent...")
-            elif option == 0:
-                print("Exiting program...")
-            else:
-                print("Invalid option. Please try again.")
-        print("Program exited.")
+        try:
+            option = -1
+            while option != 0:
+                print("1. Create Torrent")
+                print("2. Download Torrent")
+                print("0. Exit")
+                option = int(input("Select an option: "))
+                if option == 1:
+                    print("Creating torrent...")
+                elif option == 2:
+                    print("Downloading torrent...")
+                elif option == 0:
+                    print("Exiting program...")
+                else:
+                    print("Invalid option. Please try again.")
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.shutdown()
+            print("Program exited.")
 
     def shutdown(self):
         print("Shutting down program...")
+        self.announcer.stop()
         exit(0)
 
     def addTorrent(self, torrent):
